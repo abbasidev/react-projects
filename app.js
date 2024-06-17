35e9e676d778053ead69b33527b6cf4862dc3369
Abbasi Dev <abbasidev@outlook.com>
2024-06-17 23:50:25 +0330

    updated

--- app.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    document.querySelector(".go-to-top").style.display = "block";
-  } else {
-    document.querySelector(".go-to-top").style.display = "none";
-  }
-});
